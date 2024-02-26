af24e1db3cb1701169fae32c9f55529f9a6a148a
Murilo Stein <contato.mrstein@gmail.com>
2024-02-26 14:10:05 -0300

    setup environment for back-end and front-end

---
+++ server/src/test/java/com/steinmurilo/xml_keeper/XmlKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steinmurilo.xml_keeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ web/xml_keeper/src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <div>XML KEEPER</div>
+  )
+}
+
+export default App
